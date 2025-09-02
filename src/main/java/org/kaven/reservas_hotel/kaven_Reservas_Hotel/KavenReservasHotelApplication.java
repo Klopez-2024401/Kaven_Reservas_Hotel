65215aadbdf8ac49c6ef6308b10fe96b1f00aae0
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2025-09-02 15:20:52 -0600

    se agrego al repositorio

---
+++ src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/KavenReservasHotelApplication.java
@@ -0,0 +1,13 @@
+package org.kaven.reservas_hotel.kaven_Reservas_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavenReservasHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavenReservasHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/KavenReservasHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaven.reservas_hotel.kaven_Reservas_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavenReservasHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
