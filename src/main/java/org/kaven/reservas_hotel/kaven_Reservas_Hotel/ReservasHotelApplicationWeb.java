39758642b195c157f5dad767db9aa4b4b4a8881f
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2025-09-04 14:58:30 -0600

    se agrego caodio en el main

--- src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/ReservasHotelApplicationWeb.java
+++ src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/ReservasHotelApplicationWeb.java
@@ -3,7 +3,7 @@ package org.kaven.reservas_hotel.kaven_Reservas_Hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class ReservasHotelApplicationWeb {
     public static void main(String[] args) {
         SpringApplication.run(ReservasHotelApplicationWeb.class, args);
