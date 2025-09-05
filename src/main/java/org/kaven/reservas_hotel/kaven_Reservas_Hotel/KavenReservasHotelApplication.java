e1c1487c2af773146f402a3b77460e3887dc8e1e
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2025-09-05 17:18:25 -0600

    se agrego vista principal y habitaciones catalogo

--- src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/KavenReservasHotelApplication.java
+++ src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/KavenReservasHotelApplication.java
@@ -20,7 +20,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class KavenReservasHotelApplication implements CommandLineRunner {
 
     @Autowired
