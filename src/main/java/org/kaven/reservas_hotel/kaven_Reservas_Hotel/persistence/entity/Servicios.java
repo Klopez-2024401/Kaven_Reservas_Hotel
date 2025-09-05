b3a326689ec3371b38d243c70f74448f173ca81b
KennXxX <Kangel-2024458@kinal.edu.gt>
2025-09-05 17:08:24 -0600

    T30. modelo de servicios creada

--- src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/persistence/entity/Servicios.java
+++ src/main/java/org/kaven/reservas_hotel/kaven_Reservas_Hotel/persistence/entity/Servicios.java
@@ -5,19 +5,17 @@ import lombok.*;
 
 import java.util.Date;
 
-@Entity(name = "Reserva")
+@Entity(name = "Servicios")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Reserva {
+public class Servicios {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
-    private Integer id_reserva;
-    private Date fecha_entrada;
-    private Date fecha_salida;
-    private Integer id_huesped;
-    private Integer id_habitacion;
     private Integer id_servicio;
+    private String nombre_servicio;
+    private String descripcion;
+    private Double precio_servicio;
 }
